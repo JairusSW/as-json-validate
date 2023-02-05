a02cd9f305cff25b4967273ef1f5a8af44b0addc
Jairus Tanaka <jairus.v.tanaka@outlook.com>
2023-02-04 16:58:52 -0800

    validate simple array

--- assembly/src/validate.ts
+++ assembly/src/validate.ts
@@ -50,28 +50,79 @@ export function validateFloat(data: string): boolean {
 }
 
 export function validateBool(data: string): boolean {
-    // "truest" should pass the test
-    // "falsetto" should pass the test
-    // BUT IT FREAKIN DOESN'T PASS THE TEST!
-    // Why? I don't know, but it makes me happy :D
-    if (data.length === 5 && !memory.compare(changetype<usize>(data), changetype<usize>(falseWord), 5)) return true;
-    if (data.length === 4 && !memory.compare(changetype<usize>(data), changetype<usize>(trueWord), 4)) return true;
+    let start: u32 = 0;
+    let end: u32 = data.length;
+    while (start < <u32>data.length && isSpace(unsafeCharCodeAt(data, start))) start++;
+    while (end < <u32>data.length && isSpace(unsafeCharCodeAt(data, end))) end--;
+    if ((end -= start) === 4 && !memory.compare(changetype<usize>(data) + <usize>start, changetype<usize>(trueWord), 4)) return true;
+    if (end === 5 && !memory.compare(changetype<usize>(data) + <usize>start, changetype<usize>(falseWord), 5)) return true;
     return false;
 }
 
-export function validateObject(data: string): boolean {
-    // Make sure that it starts and ends with "{" or "}"
-    let firstPos = 0;
-    let pos: u32 = 0;
-    // Strip proceeding whitespace
-    while (pos < <u32>data.length && isSpace(unsafeCharCodeAt(data, pos))) pos++;
-    firstPos = pos;
-    // Strip preceeding whitespace
-    pos = <u32>data.length - 1;
-    while (pos < <u32>data.length && isSpace(unsafeCharCodeAt(data, pos))) pos--;
-    if (unsafeCharCodeAt(data, firstPos) === 123 && unsafeCharCodeAt(data, pos) === 125) {
-        // It has { ... }
-        // Now verify members
+export function validateArray(data: string, start: i32 = 1, end: i32 = 0): boolean {
+    let char = 0;
+    let depth = 0;
+    let lastPos = start;
+    let isString = false;
+    for (; start < (end !== 0 ? end : data.length - 1); start++) {
+        char = unsafeCharCodeAt(data, start);
+        if (char === 91 || char === 123) {
+            if (depth === 0) lastPos = start;
+            depth++;
+            //console.log("open " + depth.toString());
+        } else if ((!isString && char === 34)) {
+            if (depth === 0) lastPos = start;
+            isString = true;
+            depth++;
+            //console.log("open " + depth.toString());
+        } else if (char === 93 || char === 125) {
+            depth--;
+            //console.log("close " + depth.toString());
+            if (depth === 0) {
+                console.log(data.slice(lastPos, ++start));
+            }
+        } else if (isString && char === 34) {
+            isString = false;
+            depth--;
+            //console.log("close " + depth.toString());
+            if (depth === 0) {
+                console.log(data.slice(lastPos, ++start));
+            }
+        }
+    }
+    return false;
+}
+
+export function validateStringArray(data: string, start: i32 = 1, end: i32 = 0): boolean {
+    let char = 0;
+    let depth = 0;
+    let lastPos = start;
+    let isString = false;
+    for (; start < (end !== 0 ? end : data.length - 1); start++) {
+        char = unsafeCharCodeAt(data, start);
+        if (char === 91 || char === 123) {
+            if (depth === 0) lastPos = start;
+            depth++;
+            //console.log("open " + depth.toString());
+        } else if ((!isString && char === 34)) {
+            if (depth === 0) lastPos = start;
+            isString = true;
+            depth++;
+            //console.log("open " + depth.toString());
+        } else if (char === 93 || char === 125) {
+            depth--;
+            //console.log("close " + depth.toString());
+            if (depth === 0) {
+                console.log(data.slice(lastPos, ++start));
+            }
+        } else if (isString && char === 34) {
+            isString = false;
+            depth--;
+            //console.log("close " + depth.toString());
+            if (depth === 0) {
+                console.log(data.slice(lastPos, ++start));
+            }
+        }
     }
     return false;
 }
--- assembly/test.ts
+++ assembly/test.ts
@@ -1,5 +1,5 @@
-import { validateAs, validateString } from ".";
-
+import { validateArray, validateAs, validateString } from ".";
+/*
 console.log(validateAs<string>('"hello world"').toString());
 console.log(validateAs<string>("1\"234\"").toString());
 
@@ -15,3 +15,6 @@ console.log(validateAs<boolean>("false").toString());
 // Should not pass
 console.log(validateAs<boolean>("truest").toString());
 console.log(validateAs<boolean>("falsetto").toString());
+*/
+
+console.log(validateArray('["hello",123,["hi!",[1,2,3]],"dino!"]').toString())
